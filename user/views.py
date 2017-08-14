b916b8cea29439d2eaf24a498fabdab606225ff2
wangdong1993 <kelvinson@foxmail.com>
2017-08-14 07:41:19 +0000

    add validation to the registeration form

--- user/views.py
+++ user/views.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template
+import bcrypt
+from user.models import User
 from user.forms import RegisterForm
 
 user_app =  Blueprint('user_app', __name__)
@@ -10,5 +12,17 @@ def login():
 @user_app.route('/register', methods=('GET', 'POST'))
 def register():
     form = RegisterForm()
-    
+    if form.validate_on_submit():
+        salt = bcrypt.gensalt()
+        hash_password = bcrypt.hashpw(form.password.data, salt)
+        
+        user = User(
+            username=form.username.data,
+            password=form.password.data,
+            email=form.email.data,
+            first_name=form.first_name.data,
+            last_name=form.last_name.data
+            )
+        user.save()
+        return "User registered"
     return render_template('user/register.html', form=form)
